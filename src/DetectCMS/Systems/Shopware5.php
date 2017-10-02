181773de4258822bde3ec490e86d4e7ee63eccbe
Michel <micheldoens@hotmail.com>
2017-10-02 10:09:05 +0200

    Create Shopware5.php

---
+++ src/DetectCMS/Systems/Shopware5.php
@@ -0,0 +1,46 @@
+<?php
+namespace DetectCMS\Systems;
+
+class Shopware5 extends \DetectCMS\DetectCMS {
+
+	public $methods = array(
+		"changelog",
+	);
+
+	public $home_html = "";
+	public $home_headers = array();
+	public $url = "";
+
+	function __construct($home_html, $home_headers, $url) {
+    $this->home_html = $home_html;
+    $this->home_headers = $home_headers;
+		$this->url = $url;
+  }
+
+	/**
+	 * See if bootstrap.js exists, and check for Shopware
+	 * @return [boolean]
+	 */
+	public function changelog() {
+
+		if($data = $this->fetch($this->url."/themes/Backend/ExtJs/backend/base/bootstrap.js")) {
+
+			/**
+			 * Changelog always starts from the second line
+			 */
+			$lines = explode(PHP_EOL, $data);
+            if(array_key_exists(1,$lines))
+            {
+                return strpos($lines[1], "Shopware") !== FALSE;
+            }
+
+
+		}
+
+		return FALSE;
+
+	}
+
+	
+
+}
